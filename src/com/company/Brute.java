1efd03d9a6152e688af0e4486a23181006168600
Trilgon <65063208+Trilgon@users.noreply.github.com>
2020-10-16 10:30:37 +0300

    Update Brute.java

--- src/com/company/Brute.java
+++ src/com/company/Brute.java
@@ -4,11 +4,14 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 public class Brute {
     public static void force(int a, int b, String hex_pas_in) throws NoSuchAlgorithmException {
         char[] symbs = new char[5];
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        long startTime = System.currentTimeMillis();
+        long endTime;
         for (int i = a; i < b; i++) {
             symbs[0] = (char)i;
             for (int j = 97; j < 123; j++) {
@@ -22,9 +25,10 @@ public class Brute {
                             String gen = new String(symbs);
                             messageDigest.update(gen.getBytes(StandardCharsets.UTF_8));
                             byte[] digest = messageDigest.digest();
-                            String hex = String.format("%064x", new BigInteger(1, digest));
-                            if (hex.equals(hex_pas_in)){
+                            String hex = String.format("%064x", new BigInteger(1, digest));if (hex.equals(hex_pas_in)){
+                                endTime = System.currentTimeMillis();
                                 System.out.println(gen);
+                                System.out.println(TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.MILLISECONDS) + " милисекунд");
                                 System.exit(0);
                             } else {
                                 messageDigest.reset();
